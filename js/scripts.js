b3c7273c4d2e266b8cad9e6369756081dc6a2899
Ben Budinger <BenBu@mossadams.ma>
2017-06-09 15:51:25 -0700

    Add logic to pick which travel destination to select

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,29 @@ $(document).ready(function() {
       }
     }
 
-    if ()
+    if (city() && crowds() && northernHem() && temperature === 'hot') {
+      alert("Vegas baby!");
+    } if else (city() && !crowds() && !northernHem() && temperature === 'hot') {
+      alert("Cabo - Suns out Guns out")
+    } else if (city() && crowds() && northernHem() && temperature === 'temperate') {
+      alert("Rome");
+    } else if (city() && crowds() && northernHem() && temperature === 'cold') {
+      alert("Boston");
+    } else if (city() && crowds() && !northernHem() && (temperature === 'cold' || temperature === 'temperate')) {
+      alert("Sydney");
+    } else if (!city() && crowds() && northernHem() && temperature === 'hot') {
+      alert("GCanyon");
+    } else if (!city() && crowds() && northernHem() && temperature === 'temperate') {
+      alert("yellowstone");
+    } else if (!city() && !crowds() && northernHem() && temperature === 'cold') {
+      alert("mt everest");
+    } else if (!city() && !crowds() && !northernHem() && temperature === 'cold') {
+      alert("s. pole");
+    } else if (!city() && !crowds() && !northernHem() && (temperature === 'hot' || temperature === 'temperate')) {
+      alert("victoria falls");
+    } else {
+      alert("you're too picky, pick one of these places");
+    }
 
 
   });
